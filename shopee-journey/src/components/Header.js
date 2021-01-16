740fbe9cbb580aa9d07d4443ed941fa2ed2fbb3c
BBFISH <44829092+xmliszt@users.noreply.github.com>
2021-01-16 16:02:44 +0800

    feat: move leaderboard button to the right, auto formatter

--- shopee-journey/src/components/Header.js
+++ shopee-journey/src/components/Header.js
@@ -1,44 +1,53 @@
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { ArrowBack, BarChart } from '@material-ui/icons';
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { ArrowBack, BarChart } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    appBar: {
-      background: 'linear-gradient(180deg, #f44336 30%, #ff7043 90%)',
-    },
-    backButton: {
-      marginRight: theme.spacing(2),
-    },
-    leaderboardButton: {},
-    title: {
-      flexGrow: 0.8,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    background: "linear-gradient(180deg, #f44336 30%, #ff7043 90%)",
+  },
+  backButton: {
+    marginRight: theme.spacing(2),
+  },
+  leaderboardButton: {},
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 function Header() {
-
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" className={classes.backButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.backButton}
+            color="inherit"
+            aria-label="back"
+          >
             <ArrowBack />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Shopee Journey
           </Typography>
-          <IconButton edge="end" className={classes.leaderboardButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="end"
+            className={classes.leaderboardButton}
+            color="inherit"
+            aria-label="leaderboard"
+          >
             <BarChart />
           </IconButton>
         </Toolbar>
       </AppBar>
     </div>
-  )
-};
+  );
+}
 
 export default Header;
